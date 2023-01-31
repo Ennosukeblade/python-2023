003a90984128500d530fab2e626dc06b64c6c26c
Ennosukeblade <mouadhjenouiz@gmail.com>
2023-01-31 19:01:06 +0100

    Hello World

---
+++ 2-fundamentals/1-fundamentals/OP/file.py
@@ -0,0 +1,8 @@
+my_new_fav_lang = 'Python' # variable declaration, initialize string
+print(my_new_fav_lang) # log statement
+type(my_new_fav_lang) # type check
+len(my_new_fav_lang) # length check
+# single line comment
+""" multiline comment """
+
+
