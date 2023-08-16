700f722b149b8c93f930bb8ced6d3badb6f50176
Ennosukeblade <mouadhjenouiz@gmail.com>
2023-08-16 12:53:59 +0100

    Users (Shell)

--- django/django_intro/first_django_project/blogs_app/urls.py
+++ django/django_intro/first_django_project/blogs_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.root),
-    path('blogs/', views.index),
+    path('blogs/', views.index, name='index'),
     path('blogs/new', views.new),
     path('blogs/create', views.create),
     path('blogs/<int:number>', views.show),
---
+++ django/django_orm/users/users_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class User(models.Model):
+    # id primary key is automatically added by django
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
